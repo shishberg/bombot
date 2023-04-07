131e8fb663c6de7ebb25838add3c320201a158a7
David McLeish <dave@mcleish.id.au>
2023-04-07 12:25:08 +1000

    responses

--- go.mod
+++ go.mod
@@ -4,10 +4,14 @@ go 1.19
 
 require (
 	github.com/andybons/gogif v0.0.0-20140526152223-16d573594812
+	github.com/bwmarrin/discordgo v0.27.1
 	github.com/jlaffaye/ftp v0.1.0
 )
 
 require (
+	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/hashicorp/errwrap v1.0.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
+	golang.org/x/sys v0.0.0-20201119102817-f84b799fce68 // indirect
 )
--- main.go
+++ main.go
@@ -1,22 +1,92 @@
 package main
 
 import (
+	"bytes"
+	"flag"
 	"image/gif"
+	"io/ioutil"
 	"log"
 	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+var (
+	tokenFile = flag.String("token", "token.txt", "file containing the bot token")
+	guildID   = flag.String("guild-id", "", "Guild ID, or empty to register globally")
 )
 
 func main() {
-	g, err := getRadarGIF("IDR713")
+	token, err := ioutil.ReadFile("token.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
-	f, err := os.Create("IDR713.gif")
+	session, err := discordgo.New("Bot " + strings.TrimSpace(string(token)))
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer f.Close()
-	if err := gif.EncodeAll(f, g); err != nil {
+	if err := session.Open(); err != nil {
 		log.Fatal(err)
 	}
+	defer session.Close()
+
+	commands := []*discordgo.ApplicationCommand{
+		{
+			Name:        "bom",
+			Description: "BOM Sydney 128km radar",
+			Type:        discordgo.ChatApplicationCommand,
+		},
+	}
+	session.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+		switch i.ApplicationCommandData().Name {
+		case "bom":
+			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseDeferredChannelMessageWithSource,
+			})
+
+			resp := &discordgo.WebhookEdit{}
+			defer func() {
+				_, err := s.InteractionResponseEdit(i.Interaction, resp)
+				log.Println(err)
+			}()
+
+			g, err := getRadarGIF("IDR713")
+			if err != nil {
+				e := "error: " + err.Error()
+				resp.Content = &e
+				return
+			}
+			var buf bytes.Buffer
+			if err := gif.EncodeAll(&buf, g); err != nil {
+				e := "error: " + err.Error()
+				resp.Content = &e
+				return
+			}
+			// s.ChannelFileSend(ch, "IDR713.gif", &buf)
+			resp.Files = []*discordgo.File{
+				{
+					Name:        "IDR713.gif",
+					ContentType: "image/gif",
+					Reader:      &buf,
+				},
+			}
+			log.Println("done")
+		}
+	})
+
+	_, err = session.ApplicationCommandBulkOverwrite(session.State.User.ID, *guildID, commands)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer session.ApplicationCommandBulkOverwrite(session.State.User.ID, *guildID, nil)
+
+	log.Println("Running.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
+
+	log.Println("Shutting down.")
 }
